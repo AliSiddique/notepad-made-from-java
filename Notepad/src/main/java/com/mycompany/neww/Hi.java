678ea5b69a410cfdb763cf7b15fbd9409ea722a7
Alisiddique <78370889+AliSiddique@users.noreply.github.com>
2022-01-30 17:53:01 +0000

    Add files via upload

---
+++ Notepad/src/main/java/com/mycompany/neww/Hi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.neww;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+
+/**
+ *
+ * @author alisiddique
+ */
+public class Hi {
+     public static void main (String args[]){
+          LaunchPage launchpage = new LaunchPage();
+          
+   
+    }
+}
